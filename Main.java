e18069dcfe81220edb161e58bd5dff6f08619c3e
Erika Maria N. Cardoso <69882736+erikancardoso@users.noreply.github.com>
2022-04-10 04:24:51 -0400

    Update and rename TestaValor.java to Main.java

--- Main.java
+++ Main.java
@@ -1,6 +1,6 @@
 package banco;
 
-public class TestaValor {
+public class Main {
 	public static void main(String[] args) {
 		DadosDaConta contaUm = new DadosDaConta(4567,6789); // informar a agencia e o numero da conta em parametro 
 		
